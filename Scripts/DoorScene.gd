c85f59790c4bdcb35fdf20f71867a6fde7702313
CosmicBubble300 <dlalondedavidson@gmail.com>
2023-11-04 23:25:44 -0400

    We can now access the global variables (the mistake was daft and my fault - sorry)

--- Scripts/DoorScene.gd
+++ Scripts/DoorScene.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-
+# Hides door when player approaches 
 func _on_Area2D_body_entered(body):
 	hide()
 
-
+# Puts door back when player leaves 
 func _on_Area2D_body_exited(body):
 	show()
