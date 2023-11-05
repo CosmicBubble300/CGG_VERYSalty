e8310ae0add02937147f83b5516d5cb1f8341a3d
CosmicBubble300 <dlalondedavidson@gmail.com>
2023-11-05 12:35:04 -0500

    New font (delicious)

---
+++ Scenes/HUD.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# Loads global variables 
+onready var global_vars = get_node("/root/Global")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$O2Counter/O2CounterText.text = String(len(global_vars.tank_list))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
